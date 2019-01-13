4b9935a2827fff5ee7622722c55d2ac05705c310
Kevin Lohman <badpirate@gmail.com>
2019-01-12 17:11:19 -0800

    allow custom jstrap width

--- src/HTXHP.hh
+++ src/HTXHP.hh
@@ -5,6 +5,7 @@ require_once('vendor/hh_autoload.php');
 // A bootstrap head
 final class :head:jstrap extends :x:element {
   attribute :head;
+  attribute string width = "device-width";
 
   use XHPHelpers;
 
@@ -14,7 +15,7 @@ final class :head:jstrap extends :x:element {
     $head = 
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content={"width=".$this->:width.", initial-scale=1, shrink-to-fit=no"} />
         <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
